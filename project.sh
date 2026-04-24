d5e05cb7d1fb23b8a4912ff4a79795f126eccefd
aljazmc <mlinaric.aljaz@gmail.com>
2026-04-24 23:56:19 +0200

    chore: upgrade node

--- project.sh
+++ project.sh
@@ -62,7 +62,7 @@ if [[ ! -f docker-compose.yml ]]; then
     cat << EOF > docker-compose.yml
 services:
     node:
-        image: node:25.6-alpine
+        image: node:current-alpine
         working_dir: $PWD
         volumes:
             - .:$PWD
